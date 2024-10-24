d21f35bd2e2aff6149f4ae2139662f0d3dec0772
CodeModeYT <tillmann.menzer@outlook.com>
2024-10-24 14:33:35 +0200

    Re-structure

---
+++ main.py
@@ -0,0 +1,13 @@
+#Import the ev3 libraries
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+# Progrm
+ev3.speaker.say('Hello, DHL here! Your Package got delivered.')
