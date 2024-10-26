44840cd3c5260c746150da98adc72d46be119ac1
CodeModeYT <tillmann.menzer@outlook.com>
2024-10-26 14:11:41 +0200

    Added support for sound playing & example song

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#Import the ev3 libraries
+#!/usr/bin/env pybricks-micropython
+# Import the ev3 libraries
 from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor
 from pybricks.parameters import Port, Stop, Direction, Button, Color
@@ -7,7 +8,27 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 # Create your objects here.
-ev3 = EV3Brick()
+ev3 = None
+state = "play"
 
-# Progrm
-ev3.speaker.say('Hello, DHL here! Your Package got delivered.')
+def mainloop():
+    global state
+    if state == "find":
+        ev3.screen.print("find")
+        state = "follow"
+    elif state == "play":
+        ev3.screen.print(state)
+        sound.play(ev3, "audio/imperialMarch.wav") #The file needs to be 1. MONO and 2. have a SAMPLE RATE of 22050 Hz to work on the Brick!
+    else:
+        ev3.screen.print("error")
+        state = "find"
+
+def setup():
+    global ev3
+    ev3 = EV3Brick()
+    ev3.speaker.set_volume(1000) #It is limited somehwre around 256(?), this could however be bypassed
+
+if __name__ == "__main__":
+    setup()
+    while True:
+        mainloop()
---
+++ sound.py
@@ -0,0 +1,6 @@
+def say(ev3, message):
+    ev3.speaker.say(message)
+    
+
+def play(ev3, sound):
+    ev3.speaker.play_file(sound)
