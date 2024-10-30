a37bf6fe16eb28d096d6d7dec0d0e0d6cfa71c97
Mr_Comand <74143930+Mr-Comand@users.noreply.github.com>
2024-10-30 17:35:09 +0100

    Refactored find_house function to simplify logic and reduce variables

--- find_house.py
+++ find_house.py
@@ -1,40 +1,19 @@
+from pybricks.tools import wait
 def find_house(us, usm):
     angle = 0
-    deg_sec = 40 # Winkelgeschwindigkeit
-    degree = 20 # Drehwinkel
-    
-    usm.run_angle(deg_sec, 0)
-    dis1 = us.distance()
-    usm.run_angle(deg_sec, degree)
-    angle += degree
-    dis2 = us.distance()
+    endAngle = 150
+    deg_sec= 100
+    degreePerStep = 5 # Drehwinkel
 
-    winkel = []
-    distance = []
-    while angle < 140:
+    minAngle = -1
+    minDistance = 20000
+    for angle in range(angle, endAngle, degreePerStep):
+        usm.run_target(deg_sec, angle*56/12)
+        dis1 = us.distance()
+        if  dis1 < minDistance:
+            minDistance = dis1
+            minAngle = angle
         print(angle)
-        if dis1 or dis2 < 150:
-            while dis1 > dis2:
-                dis1 = dis2
-                angle += degree
-                usm.run_angle(deg_sec, degree)
-                dis2 = us.distance()
-            winkel.append(angle)
-            while dis1 < dis2:
-                dis1 = dis2
-                angle += degree
-                usm.run_angle(deg_sec, degree)
-                dis2 = us.distance()
-                print("l2")
-            print("if")
-        else:
-            dis1 = dis2
-            angle += degree
-            usm.run_angle(deg_sec, degree)
-            dis2 = us.distance()
-            print("else")
-        
-    small_dis = min(distance)
-    ind_dis = distance.index(small_dis)
-    small_winkel = winkel[ind_dis]
-    return small_winkel
+        wait(100)
+    
+    return minAngle
