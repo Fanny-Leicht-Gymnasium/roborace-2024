3d3f9930b0f8aabe818579e7f56322f865787f8c
Mr_Comand <74143930+Mr-Comand@users.noreply.github.com>
2024-11-07 20:22:31 +0100

    Updated find_house.py and main.py with changes to angle calculations, motor targets, and sensor readings.

--- find_house.py
+++ find_house.py
@@ -1,19 +1,19 @@
 from pybricks.tools import wait
 def find_house(us, usm):
-    angle = 0
-    endAngle = 150
+    angle = 75-75
+    endAngle = 75+75
     deg_sec= 100
     degreePerStep = 5 # Drehwinkel
 
     minAngle = -1
-    minDistance = 20000
+    minDistance = 2000000
     for angle in range(angle, endAngle, degreePerStep):
-        usm.run_target(deg_sec, angle*56/12)
+        usm.run_target(deg_sec, angle*56/24)
         dis1 = us.distance()
         if  dis1 < minDistance:
             minDistance = dis1
             minAngle = angle
-        print(angle)
+        print(angle, dis1)
         wait(100)
     
     return minAngle
--- main.py
+++ main.py
@@ -29,40 +29,57 @@ us = UltrasonicSensor(Port.S4)
 last = time.time()-10
 
 def setup():
-    us_motor.run_target(speed=100, target_angle=90*56/12)
+    us_motor.run_target(speed=100, target_angle=-90*56/24)
 
 if __name__ =="__main__":
     # setup when starting the program
     setup()
     dt = time.time() - last
     last = time.time()
-    
     # drive forward until red line
+    # while True:'
+    #     None'
+    #     driver.drive(-50, 0)
+    
     while not color.checkColor(Color.RED):
         seiten_regler.seiten_regler(ev3, dt, us, driver, wall_distance)
     
     driver.stop()
+    driver.turn(-15*40/16)
     # when red line: find the direciton in which the house is located
     house_angle = find_house.find_house(us, us_motor)
-    
+    print("--"*30)
+    print(house_angle)
     # turn the robot in the direction of the house
-    driver.turn(house_angle)
-    
+    driver.turn(-house_angle*40/16)
+    ev3.speaker.beep()
+    wait(100)
+    us_motor.run_target(1000, 0)
+    ev3.speaker.beep()
+    wait(100)
     # drive forward until house wall reached (= until distance to the house is <= 20mm)
-    while not us.distance() <= 20:
-        seiten_regler.seiten_regler(ev3, dt, us, driver, wall_distance)
+    while not us.distance() <= 200:
+        driver.drive(-50, 0)
     
     driver.stop()
     # turn robot 90 degrees to the left
-    driver.turn(90)
-    
+    # driver.turn(90)
+    driver.turn(-90*40/16)
+    us_motor.run_target(speed=100, target_angle=-90*56/24)
+    ev3.speaker.beep()
+    wait(100)
     # drive forward until green line
     while not color.checkColor(Color.GREEN):
+        print("Aktuell: ",colorSensor.color(),colorSensor.rgb()," Soll: ",Color.GREEN)
         seiten_regler.seiten_regler(ev3, dt, us, driver, wall_distance)
         
     # draw a smiley and stop
     driver.stop()
+    us_motor.run_target(speed=100, target_angle=-0*56/24)
     screen.smiley(ev3)
+    while True:
+        ev3.speaker.beep()
+        wait(100)
     
         
         
