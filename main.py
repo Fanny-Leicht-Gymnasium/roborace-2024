7fab4f83ec0d7b9b3191ebc9365e74eef565d74a
CodeModeYT <tillmann.menzer@outlook.com>
2024-11-18 11:43:08 +0100

    Added improved color check (not tested yet!)

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ wheel_diameter = 66.8
 axle_track = 14
 wall_distance = 200 # min distance to walls in mm
 
+# color check
+red = (1, 1, 1)
+green = (1, 1, 1)
+tolerance = (1, 1, 1)
 
 # system vars
 ev3 = EV3Brick()
@@ -27,6 +31,14 @@ driver = DriveBase(left_motor, right_motor, wheel_diameter=wheel_diameter, axle_
 colorSensor = ColorSensor(Port.S1)
 us = UltrasonicSensor(Port.S4)
 last = time.time()-10
+
+# functions
+def is_color_in_range(measured_color, target_color, tolerance):
+    return all(abs(measured_color[i] - target_color[i]) <= tolerance for i in range(3))
+
+def checkColor(target_color):
+    measured_rgb = colorSensor.rgb()
+    return is_color_in_range(measured_rgb, target_color, tolerance)
 
 def setup():
     us_motor.run_target(speed=100, target_angle=90*56/24)
@@ -41,8 +53,9 @@ if __name__ =="__main__":
     #     None'
     #     driver.drive(-50, 0)
     
-    while not color.checkColor(Color.RED):
+    while not checkColor(red):
         seiten_regler.seiten_regler(ev3, dt, us, driver, wall_distance)
+        print(checkColor(red))
     
     driver.stop()
     driver.turn(15*40/16)
@@ -69,8 +82,8 @@ if __name__ =="__main__":
     ev3.speaker.beep()
     wait(100)
     # drive forward until green line
-    while not color.checkColor(Color.RED):
-        print("Aktuell: ",colorSensor.color(),colorSensor.rgb()," Soll: ",Color.GREEN)
+    while not color.checkColor(green):
+        print(checkColor(green))
         seiten_regler.seiten_regler(ev3, dt, us, driver, wall_distance)
         
     # draw a smiley and stop
